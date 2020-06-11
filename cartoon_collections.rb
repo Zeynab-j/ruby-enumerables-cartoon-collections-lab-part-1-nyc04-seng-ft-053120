781487b70906a7b857022dbfadbaa02cc1ab3a96
Zeynab Jibreel <zeina2025@gmail.com>
2020-06-11 16:04:01 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -13,6 +13,6 @@ def list_dwarves(array)
   # Print a numbered list of each element
   dwarves_array = ["Dopey", "Grumpy", "Bashful"]
   dwarves_array.each_with_index { |element, index|
-   puts " #{index} #{element}" }
+   puts " #{index} 1 #{element}" }
 end
 end
